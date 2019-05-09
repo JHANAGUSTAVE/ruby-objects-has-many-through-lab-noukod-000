c7ed0fdebd0aa4154f0d55853c4372641c562254
Jhana Gustave <jhana.gustave@gmail.com>
2019-05-09 14:58:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,23 @@
-class Song 
-  attr_accessor :name, :artist, :genre
-  @@all = []
-  def initialize(name, genre)
-    @name = name 
-    @artist = artist 
-    genre.add_song(self)
-  end 
- 
-end
+
+ class Song
+
+   attr_accessor :name, :artist, :genre
+
+   ALL = []
+
+   def initialize(name="", artist="", genre="")
+    @name = name
+    @artist = artist
+    @genre = genre
+    ALL << self
+  end
+
+   def self.all
+    ALL
+  end
+
+   def artist
+    @artist
+  end
+
+ end
